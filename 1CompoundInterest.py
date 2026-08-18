04aa1c32f0cf43856c868a2f1ad636d6317b7441
Aaron <aaroncheong255@gmail.com>
2026-08-18 17:02:12 +0800

    Refactor compound interest calculation with user input
    Updated the script to allow user input for balance, annual interest rate, monthly contributions, and years. Changed the calculation to work on a yearly basis instead of monthly.

--- 1CompoundInterest.py
+++ 1CompoundInterest.py
@@ -1,14 +1,22 @@
 #interest= balance x monthly rate 
 #new balance = balance + interest + contributions 
+#Input your own balance, rates , contributions, years
 
-balance = 1000
-monthly_rate = 0.01
-contributions = 100
-months = 12
+balance = float(input("Starting Balance: "))
+annual_rate = float(input("Annual Interest Rate(%): "))
+contributions = float(input("Monthly Contributions: "))
+years = int(input("Years: "))
 
-for month in range(1, months +1):
-    interest = balance * monthly_rate
-    balance = balance + interest + contributions
+annual_rate_adjusted = annual_rate / 100
+yearly_contributions = contributions * 12 
+
+for i in range(1, years +1):#+1 means years increment of +1
+    interest = balance * annual_rate_adjusted
+    balance = balance + interest + yearly_contributions
     #f-string drop variables inside{}
     #The :.2f shows a flaot with 2 decimal places 
-    print(f"Month {month}: interest {interest:.2f}, balance ${balance:.2f}")
+    print(f"Year {i}: Interest ${interest:.2f}, Cumulative Balance ${balance:.2f}")
+    #print i because i is the loop
+
+
+    #next idea is to add a total contributions 
